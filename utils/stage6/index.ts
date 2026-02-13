a6031c019ebce6940c312dd731048eaa744f8a73
growth-mvp <ishihara@centerboard.co.jp>
2026-02-13 14:20:23 +0900

    Create index.ts

---
+++ utils/stage6/index.ts
@@ -0,0 +1,30 @@
+/**
+ * STAGE6 utilities re-export
+ */
+
+export type {
+  ApprovedProject,
+  ProjectContribution,
+  NorthStarRow,
+  IssueResolution,
+  Stage6Core,
+} from './types';
+
+export { mkBaseFigures, mkBaselineTrajectory } from './baseline';
+
+export {
+  fmtJPY,
+  compactJPY,
+  extractMetricFromYearlyPL,
+  calculateAchievementRate,
+  getTopContributingProjects,
+  getEvidenceFromProject,
+  diffYearly,
+  sumYearly,
+  buildNorthStarRows,
+  buildIssueResolutions,
+  buildValueAnalysisCards,
+  buildProjectContributions,
+} from './compute';
+
+export { getExecutionWeight } from './execution';
