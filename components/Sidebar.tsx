f2bd7adc6fd9c705d15d81bc8368671da92bac68
growth-mvp <ishihara@centerboard.co.jp>
2026-06-18 14:02:36 +0900

    Update Sidebar.tsx

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -116,7 +116,7 @@ export default function Sidebar() {
             <img
               src="/GROWTH SHIFT.png"
               alt="GROWTH Logo"
-              className="block h-auto w-[270px] max-w-none object-contain transition-transform hover:scale-[1.02]"
+              className="block h-auto w-[235px] max-w-none object-contain transition-transform hover:scale-[1.02]"
             />
           </Link>
         </div>
