eaa0ca329b2dd2a3655e7770005b1bdd55ef15bf
growth-mvp <ishihara@centerboard.co.jp>
2026-07-07 10:51:14 +0900

    Create page.tsx

---
+++ app/stage0/page.tsx
@@ -0,0 +1,8 @@
+// /app/stage0/page.tsx
+'use client';
+
+import Stage0Container from '@/components/stage0/Stage0Container';
+
+export default function Stage0Page() {
+  return <Stage0Container />;
+}
